5f0a7c6c79aef5c56319f62f02bf43ad8179a83c
bboddyya <krug116@yandex.ru>
2022-06-08 23:10:30 +0400

    Добавил линк на посты, переход на url по id

---
+++ f
@@ -0,0 +1,41 @@
+[33mcommit 644acbfef9f28369a3a183fe671592f9931c2d24[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m)[m
+Author: bboddyya <krug116@yandex.ru>
+Date:   Wed Jun 8 17:57:22 2022 +0400
+
+    Перед созданием перехода в отдельный пост
+
+[33mcommit 163b4739b6b1b3b5352b513b8c80f87c81a96b6e[m
+Author: bboddyya <krug116@yandex.ru>
+Date:   Wed Jun 8 16:46:37 2022 +0400
+
+    Hover на постах, посты без переноса, троеточие в конце строки
+
+[33mcommit 15a33d2d2adb4ac8bbf3315413d343d132cedf71[m
+Author: bboddyya <krug116@yandex.ru>
+Date:   Wed Jun 8 15:25:36 2022 +0400
+
+    Аватарка отсутствия фото
+
+[33mcommit a3db7697d31f7308b575f0903002e63eb57d40e3[m[33m ([m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: bboddyya <krug116@yandex.ru>
+Date:   Tue Jun 7 19:59:58 2022 +0400
+
+    Добавил верстку аватарок
+
+[33mcommit 1722b3d900218e526749d6d2b1d6c5d0e1cf691a[m
+Author: bboddyya <krug116@yandex.ru>
+Date:   Tue Jun 7 18:43:59 2022 +0400
+
+    Додедал темную тему, добавил аватарки
+
+[33mcommit 4443488dd46cee775f40aa65f6cd9bea21360e18[m
+Author: bboddyya <102018823+bboddyya@users.noreply.github.com>
+Date:   Sun Jun 5 13:21:01 2022 +0400
+
+    Initial commit
+
+[33mcommit bd5e56c11ff88cfafa36085d214b7f72bcd6d93c[m
+Author: bboddyya <102018823+bboddyya@users.noreply.github.com>
+Date:   Sun Jun 5 13:20:49 2022 +0400
+
+    Initial commit
--- src/Pages/Tasks/UI/Post.js
+++ src/Pages/Tasks/UI/Post.js
@@ -22,56 +22,56 @@ function Post() {
             picture,
           }) => {
             return (
-              // <Link to={"/tasks/id"}>
-              <div
-                key={id}
-                className="post"
-                style={darkMode ? themeColor.post : null}
-              >
-                <div className="headerWrapper">
-                  <div className="pictureWrapper">
-                    {picture ? (
-                      <img
-                        src={picture}
-                        alt=""
-                        className="picture"
-                        style={darkMode ? themeColor.pictureBorder : null}
-                      />
-                    ) : (
-                      <div
-                        className="lnr lnr-user profilePicture"
-                        style={darkMode ? themeColor.profilePicture : null}
-                      />
-                    )}
+              <Link to={"/tasks/" + id}>
+                <div
+                  key={id}
+                  className="post"
+                  style={darkMode ? themeColor.post : null}
+                >
+                  <div className="headerWrapper">
+                    <div className="pictureWrapper">
+                      {picture ? (
+                        <img
+                          src={picture}
+                          alt=""
+                          className="picture"
+                          style={darkMode ? themeColor.pictureBorder : null}
+                        />
+                      ) : (
+                        <div
+                          className="lnr lnr-user profilePicture"
+                          style={darkMode ? themeColor.profilePicture : null}
+                        />
+                      )}
+                    </div>
+                    <div className="postHeader">{author}</div>
                   </div>
-                  <div className="postHeader">{author}</div>
-                </div>
 
-                <div className="postTitle">{title}</div>
-                <div
-                  className="postDate"
-                  style={darkMode ? themeColor.likesShareRepost : null}
-                >
-                  {time}
-                  {" · "}
-                  {date}, 2022
+                  <div className="postTitle">{title}</div>
+                  <div
+                    className="postDate"
+                    style={darkMode ? themeColor.likesShareRepost : null}
+                  >
+                    {time}
+                    {" · "}
+                    {date}, 2022
+                  </div>
+                  <div
+                    className="likesShareRepost"
+                    style={darkMode ? themeColor.likesShareRepost : null}
+                  >
+                    <span className="likes">
+                      <strong>{likes}</strong> Лайков
+                    </span>
+                    <span className="replies">
+                      <strong>{replies}</strong> Ответов
+                    </span>
+                    <span className="shares">
+                      <strong>{shares}</strong> Репостов
+                    </span>
+                  </div>
                 </div>
-                <div
-                  className="likesShareRepost"
-                  style={darkMode ? themeColor.likesShareRepost : null}
-                >
-                  <span className="likes">
-                    <strong>{likes}</strong> Лайков
-                  </span>
-                  <span className="replies">
-                    <strong>{replies}</strong> Ответов
-                  </span>
-                  <span className="shares">
-                    <strong>{shares}</strong> Репостов
-                  </span>
-                </div>
-              </div>
-              // </Link>
+              </Link>
             );
           }
         )
